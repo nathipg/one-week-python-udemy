4eaadc2f8d93e269e9d23a7990255b477713d25b
Nathália Pissuti <nathaliapissuti@gmail.com>
2025-03-19 22:25:43 -0300

    More strings

---
+++ 6.29_string_slice.py
@@ -0,0 +1,8 @@
+giant_woman = 'Giant Woman';
+
+print(giant_woman[0:5]) # Doesn't include the index 5
+print(giant_woman[:5]) # From start until index 5
+print(giant_woman[6:]) # Until the end
+print(giant_woman[0:99])
+
+print(giant_woman[::2]) # With step
---
+++ 6.31_escape.py
@@ -0,0 +1,3 @@
+phrase = "hello\nworld"
+
+print(phrase)
